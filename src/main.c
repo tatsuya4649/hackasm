72acc96379376cbb3b9602c6aa77c5b0a190fde5
tatsuya4649 <tatsuya4946developer@gmail.com>
2020-12-15 16:33:16 +0900

    a

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 int main(int argc,const char *argv[])
 {
-	printf("%d\n",argc);
-	printf("%s\n",argv[1]);
+	const char *file;
+	if ( !argv[1] || argc<=1 )
+		printf("error : not found to .asm file\n");
+	/* get file name */
+	file = argv[1];
 }
